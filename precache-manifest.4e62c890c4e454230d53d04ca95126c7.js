6197f48f6842c58879a721f126a221f243ddd595
tejavenkatlanka <tvlanka9@gmail.com>
2018-12-17 00:55:21 +0530

    web host init 1

---
+++ precache-manifest.4e62c890c4e454230d53d04ca95126c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05773d53a80748feb222",
+    "url": "/web/static/js/runtime~main.05773d53.js"
+  },
+  {
+    "revision": "3653f283ff2d96a2d5b9",
+    "url": "/web/static/js/main.3653f283.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/web/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "3653f283ff2d96a2d5b9",
+    "url": "/web/static/css/main.fac7c481.chunk.css"
+  },
+  {
+    "revision": "643be88f9fef596fe6624db4ce460074",
+    "url": "/web/index.html"
+  }
+];
