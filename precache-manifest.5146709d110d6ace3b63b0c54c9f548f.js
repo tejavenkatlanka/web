76412d384255eed3234c00b7dff63ced6aee255b
tejavenkatlanka <tvlanka9@gmail.com>
2018-12-18 00:17:45 +0530

    Updates

---
+++ precache-manifest.5146709d110d6ace3b63b0c54c9f548f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f27f8a18ba4cf808b6c6",
+    "url": "/static/js/main.f27f8a18.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "f27f8a18ba4cf808b6c6",
+    "url": "/static/css/main.a7406322.chunk.css"
+  },
+  {
+    "revision": "773af30d5af1c8383925ff095d477f0a",
+    "url": "/index.html"
+  }
+];
